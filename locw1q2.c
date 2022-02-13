2ea4dd5da59129a7c0f36365aa62af88c7519677
Chirag Khaitan <61582682+chiragkhaitan@users.noreply.github.com>
2022-02-13 13:35:50 +0530

    Add files via upload

---
+++ locw1q2.c
@@ -0,0 +1,13 @@
+// Q2
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter two numbers");
+    scanf("%d%d",&a,&b);
+    if(a>b){
+        printf("a is greater");
+    }else if(b>a){
+        printf("b is greater");
+    }
+    return 0;
+}
